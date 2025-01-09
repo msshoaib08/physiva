4b8c347d55dbe3b9bdfe17d4a94b8c04288a232f
Shoaib <ms.shoaib23@gmail.com>
2025-01-09 17:09:28 +0530

    Update the google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,11 @@ export default function RootLayout({
 					content="q6JlAdAHDVzHWgwgyDDz0r4185XxykdhVYuvAYdhHTM"
 				/>
 
+				<meta
+					name="google-site-verification"
+					content="cHGpGUd0NCUg51Ndtdrrvp8cbEQrMOqRaQdh9aOFZwM"
+				/>
+
 				<script type="application/ld+json">
 					{JSON.stringify({
 						'@context': 'https://schema.org',
